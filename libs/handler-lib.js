32098da7a175ea3f688d1c847e6fae74e6084296
stephan.oehrli <stephan.oehrli@adesso.ch>
2021-07-05 17:21:24 +0200

    Adding serverless API

---
+++ libs/handler-lib.js
@@ -0,0 +1,16 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    return { statusCode, body: JSON.stringify(body) };
+  };
+}
